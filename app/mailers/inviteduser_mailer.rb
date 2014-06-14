de61bd4d77310f1cff17f2c964046cf25e39b5e4
plattyp <andrew.platkin@gmail.com>
2014-06-13 19:23:43 -0500

    worked on mailer now with url link that works

--- app/mailers/inviteduser_mailer.rb
+++ app/mailers/inviteduser_mailer.rb
@@ -1,5 +1,6 @@
 class InviteduserMailer < ActionMailer::Base
   default from: "contact@platty.co"
+  default_url_options[:host] = 'localhost:3000'
 
   def invited_email(email)
   	mail(to: email, subject: 'You''ve been invited to collaborate on an idea!')
--- app/models/inviteduser.rb
+++ app/models/inviteduser.rb
@@ -2,6 +2,9 @@ class Inviteduser < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :idea
 
+	#Basic email validation
+	validates :emailaddress, :email => { :allow_nil => false, :message => 'Not a valid email address' }
+
 	def self.return_invitedusers(idea_id, emails)
 		Inviteduser.where("idea_id = ? AND emailaddress not in (?)",idea_id,emails).select("emailaddress","role")
 	end
