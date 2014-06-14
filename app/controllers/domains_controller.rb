d8b30f69ec207c5bf65931673f77928bbee2907c
plattyp <andrew.platkin@gmail.com>
2014-06-14 09:58:08 -0500

    started work on domain search

--- app/controllers/domains_controller.rb
+++ app/controllers/domains_controller.rb
@@ -1,12 +1,17 @@
 class DomainsController < ApplicationController
 	respond_to :html, :xml, :json
 	before_action :get_idea, :get_user
+	require 'whois'
 
 	def index
-		if params[:search]
-			@domains = Domain.search(params[:search])
-		else
-			@domains = Domain.all
+		unless params[:search].blank?
+			@domainsearch = Whois.whois(params[:search])
+
+			@domainsearch.each do |s|
+				@domainresults.url = s.domain
+				@domainresults.status = s.status
+				@domainresults.expirationdate = s.expires_on
+			end
 		end
 	end
 
@@ -16,21 +21,34 @@ class DomainsController < ApplicationController
 
 	def create
 		@domain = @idea.domains.build(domain_params)
-		@domain.ideadomains.build(:user_id => @user.id, :idea => @idea)
+
+		#Setting attribute values now until this actually looks up the info from Whois
+		@domain.name = params[:url]
+		@domain.url = params[:url]
+		@domain.domainstatus_id = "Added"
+		@domain.expirationdate = Time.zone.now.to_date
+
 		#Search existing domains to see if the domain exists
 		@domainsearch = Domain.search(@domain.url)
-
-		if @domainsearch.blank?
-			render :action => "new", :notice => "No results found"
+		
+		if @domainsearch.count === 0
+			#Add the domain to the database
+			if @domain.save
+				#After the domain has been added to the database successfully, add the association for the given idea
+				@ideadomain = Ideadomain.create(:idea_id => @idea.id, :domain_id => @domain.id, :user_id => @user.id)
+			else
+				redirect_to idea_domain_path(@idea), :notice => "The domain was unable to be added to your watchlist"
+			end
 		else
-			render :action => "new", :notice => "We found a result!"
+			#If the domain already exists, just create the association to add to the watchlist
+			@ideadomain = Ideadomain.create(:idea_id => @idea.id, :domain_id => domainsearch.id, :user_id => @user.id)
 		end
 	end
 
 	private
 
 	def domain_params
-		params.require(:domain).permit(:url, ideadomain_attributes: [:idea_id, :user_id])
+		params.fetch(:domain, {}).permit(:url)
 	end
 
 	def get_idea
--- app/controllers/ideadomains_controller.rb
+++ app/controllers/ideadomains_controller.rb
@@ -1,2 +1,23 @@
 class IdeadomainsController < ApplicationController
+	respond_to :html, :xml, :json
+	before_action :get_idea
+	
+	def Create
+		@ideadomain = @idea.ideadomain.build(ideadomain_params)
+		if @ideadomain.save
+			redirect_to idea_domain_path(@idea), :notice => "The domain was added to your watchlist!"
+		else
+			redirect_to idea_domain_path(@idea), :notice => "The domain was unable to be added to your watchlist"
+		end
+	end
+
+	private
+
+	def ideadomain_params
+		params.require(:ideadomain).permit(:domain_id, :idea_id, :user_id)
+	end
+
+	def get_idea
+		@idea = Idea.find(params[:idea_id])
+	end
 end
