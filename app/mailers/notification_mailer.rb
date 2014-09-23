2308dedf5f241ab0ae2777dc47d91e5ab6df358e
plattyp <andrew.platkin@gmail.com>
2014-09-23 06:58:52 -0500

    shows who sent the message in the email subject line

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -3,7 +3,14 @@ class NotificationMailer < ActionMailer::Base
   default_url_options[:host] = ENV["DEFAULT_URL_MAIL_OPTIONS"]
 
   def notification_email(email,body)
-  	@body = body
-  	mail(to: email, subject: 'You have received a notification on an idea')
+  	# Set the notification properties
+  	case body.notifiable_type
+  	when "Ideamessage"
+  		messageinfo = Ideamessage.return_message_info(body.notifiable_id)
+  		subject = "You have received a new message from #{messageinfo.sender}"
+  	end
+
+  	# Send out the email
+  	mail(to: email, subject: subject)
   end
 end
--- app/models/ideamessage.rb
+++ app/models/ideamessage.rb
@@ -6,4 +6,8 @@ class Ideamessage < ActiveRecord::Base
 
 	validates :message, :presence => true
 	default_scope order('created_at DESC')
+
+	def self.return_message_info(message_id)
+		Ideamessage.joins(:user).where("ideamessages.id = ?",message_id).select("users.username as sender","idea_id","ideamessages.created_at").first
+	end
 end
